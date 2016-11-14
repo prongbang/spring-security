8bd1dc69c54a2ff499c09037c4e2ab3af04dbc18
prongbang <prongbang@gmail.com>
2016-11-14 11:47:11 +0700

    Spring MVC + Thymeleaf + Spring Security

---
+++ src/main/java/com/prongbang/sec/controller/HomeController.java
@@ -0,0 +1,30 @@
+package com.prongbang.sec.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+
+	@RequestMapping(method = RequestMethod.GET)
+	public String init(ModelMap model) { 
+		
+		return "index";
+	}
+	
+    @RequestMapping(value = "/admin", method = RequestMethod.GET)
+    public String adminPage(ModelMap model) { 
+		
+        return "admin";
+    }
+ 
+    @RequestMapping(value = "/db", method = RequestMethod.GET)
+    public String dbaPage(ModelMap model) { 
+		
+        return "dba";
+    }
+	
+}
